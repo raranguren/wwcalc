5a90267abf759b3b9f7cb8caa565569b31d41c79
raranguren <ricaragas@hotmail.com>
2024-02-01 18:16:36 +0000

    Fixed directory for assets

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import { AppComponent } from './app/app.component';
 import { APP_BASE_HREF } from '@angular/common';
 
 bootstrapApplication(AppComponent, {
-  providers: [{ provide: APP_BASE_HREF, useValue: '.' }]
+  providers: [{ provide: APP_BASE_HREF, useValue: './' }]
 })
   .catch((err) => console.error(err));
