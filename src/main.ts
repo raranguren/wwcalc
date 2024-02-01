5a57d0d5748ba2758270acbc4b5da56569de8f88
raranguren <ricaragas@hotmail.com>
2024-02-01 18:23:52 +0000

    Moved base URL setting from main.ts to angular.json

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { APP_BASE_HREF } from '@angular/common';
 
-bootstrapApplication(AppComponent, {
-  providers: [{ provide: APP_BASE_HREF, useValue: './' }]
-})
+bootstrapApplication(AppComponent)
   .catch((err) => console.error(err));
