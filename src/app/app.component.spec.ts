9636633b73b8fe0e530a0c506f7bf3c323133e24
raranguren <ricaragas@hotmail.com>
2024-02-04 22:43:48 +0100

    Adds Jest (#1)
    * Migrate from Karma/Jasmine to Jest
    
    * Add test coverage report to gh-pages

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create the app', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should render the title', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain('Werewolf');
+  });
+});
